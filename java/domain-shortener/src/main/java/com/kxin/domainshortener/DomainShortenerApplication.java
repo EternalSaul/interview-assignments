162912da8cf33b29cd46b8daa2d78ac376715a77
Kuan Xin <eternx@163.com>
2022-03-15 21:38:06 +0800

    init a spring-boot start project

---
+++ java/domain-shortener/src/main/java/com/kxin/domainshortener/DomainShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.kxin.domainshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainShortenerApplication.class, args);
+	}
+
+}
---
+++ java/domain-shortener/src/test/java/com/kxin/domainshortener/DomainShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kxin.domainshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
