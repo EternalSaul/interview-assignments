92cd4aac8297c8cb975dcea18a6f7baa721f954c
Kuan Xin <eternx@163.com>
2022-03-16 20:30:25 +0800

    init springfox swagger config

--- java/domain-shortener/src/main/java/com/kxin/domainshortener/DomainShortenerApplication.java
+++ java/domain-shortener/src/main/java/com/kxin/domainshortener/DomainShortenerApplication.java
@@ -2,8 +2,10 @@ package com.kxin.domainshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
+@EnableOpenApi
 public class DomainShortenerApplication {
 
 	public static void main(String[] args) {
---
+++ java/domain-shortener/src/main/java/com/kxin/domainshortener/controller/DomainShortenController.java
@@ -0,0 +1,9 @@
+package com.kxin.domainshortener.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "domain-shorten")
+public class DomainShortenController {
+}
